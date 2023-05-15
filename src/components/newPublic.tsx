445786eb4040db597d86c4a7c45d2a42fb3d4835
Murylo Andrade <muryloandrades@gmail.com>
2023-05-15 12:55:15 -0300

    modal publication

--- src/components/newPublic.tsx
+++ src/components/newPublic.tsx
@@ -1,9 +1,15 @@
 import { Button, Input, Modal, TextareaAutosize } from "@material-ui/core";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import UploadButton from "./UploadButton/UploadButton";
+import axios from "axios";
 
 export const NewPublic = () => {
   const [ModalOpen, setModalOpen] = useState(false);
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [selectCourse, setSelectCourse] = useState<string>("");
+  const [selectCategory, setSelectCategory] = useState<string>("");
+  const [contentPost, setContentPost] = useState<string>("");
+  const [name, setName] = useState<string>("");
 
   const handleModalClose = () => {
     setModalOpen(false);
@@ -12,6 +18,66 @@ export const NewPublic = () => {
   const handleOpen = () => {
     setModalOpen(true);
   };
+
+  const SavePost = useCallback(() => {
+    const data = new FormData();
+    axios.post("http://localhost:7010/posts", {
+      nameUser: name,
+      date: data,
+      role: selectCategory,
+      Course: selectCourse,
+      content: contentPost,
+      photo: "https://picsum.photos/200/300/?random",
+      likes: 0,
+    });
+  }, [name, selectCategory, selectCourse, contentPost]);
+
+  const courser = [
+    "Administração",
+    "Agronomia",
+    "Arquitetura e Urbanismo",
+    "Artes Visuais",
+    "Biomedicina",
+    "Ciência da Computação",
+    "Ciências Biológicas",
+    "Ciências Contábeis",
+    "Ciências Econômicas",
+    "Design",
+    "Direito",
+    "Educação Física",
+    "Enfermagem",
+    "Engenharia Ambiental e Sanitária",
+    "Engenharia Civil",
+    "Engenharia de Produção",
+    "Engenharia Elétrica",
+    "Engenharia Mecânica",
+    "Engenharia Química",
+    "Estética e Cosmética",
+    "Farmácia",
+    "Fisioterapia",
+    "Fonoaudiologia",
+    "Gastronomia",
+    "Gestão Comercial",
+    "Gestão de Recursos Humanos",
+    "Gestão Financeira",
+    "Jornalismo",
+    "Letras",
+    "Logística",
+    "Marketing",
+    "Medicina Veterinária",
+    "Nutrição",
+    "Odontologia",
+    "Pedagogia",
+    "Psicologia",
+    "Publicidade e Propaganda",
+    "Radiologia",
+    "Relações Internacionais",
+    "Relações Públicas",
+    "Serviço Social",
+    "Sistemas de Informação",
+    "Teologia",
+    "Turismo",
+  ];
 
   return (
     <>
@@ -71,6 +137,42 @@ export const NewPublic = () => {
               <p>Comece uma nova Publicação</p>
               <Button style={{ color: "white" }}>X</Button>
             </div>
+            <Input
+              placeholder="qual o seu nome?"
+              onChange={(event) => setName(event.target.value)}
+            />
+            <p
+              style={{
+                marginTop: "10px",
+              }}
+            >
+              Selecione sua categoria:
+            </p>
+            <select
+              style={{
+                width: "100%",
+                border: "none",
+                outline: "none",
+                marginBottom: "10px",
+              }}
+            >
+              <option value="Professor">Professor</option>
+              <option value="Aluno">Aluno</option>
+            </select>
+            <p>Qual o seu curso?</p>
+            <select
+              style={{
+                width: "100%",
+                border: "none",
+                outline: "none",
+              }}
+            >
+              {courser.map((item) => (
+                <option value={item} onChange={() => setSelectCourse(item)}>
+                  {item}
+                </option>
+              ))}
+            </select>
             <TextareaAutosize
               placeholder="O que deseja compartilhar hoje?"
               style={{
@@ -87,6 +189,7 @@ export const NewPublic = () => {
                 backgroundColor: "white",
                 marginTop: "10px",
               }}
+              onClick={() => SavePost}
             >
               Publicar
             </Button>
